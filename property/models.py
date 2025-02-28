daa1b6de9001873bf2e6da6ca75e26af58296d26
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-02-28 20:53:42 +0600

    setup Flat changelist page.

--- property/models.py
+++ property/models.py
@@ -12,7 +12,7 @@ class Flat(models.Model):
 
     description = models.TextField('Текст объявления', blank=True)
     price = models.IntegerField('Цена квартиры', db_index=True)
-
+    new_building = models.BooleanField(verbose_name='Новостройка', default=None, null=True)
     town = models.CharField(
         'Город, где находится квартира',
         max_length=50,
