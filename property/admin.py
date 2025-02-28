ee693b7e1942109357a40b9542b15b4f3cdd9d1e
golovolom288 <142310490+golovolom288@users.noreply.github.com>
2025-02-28 20:34:55 +0600

    поправил admin.py

--- property/admin.py
+++ property/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from property.models import Flat
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
-
-
-admin.site.register(Flat, FlatAdmin)
